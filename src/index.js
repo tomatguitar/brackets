911dd0d71744bdb971b22737e0864ea2e0b6e6c8
tomatguitar <zkarman@mail.ru>
2020-10-10 18:31:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(input, config) {
+  const flattened = config.flatMap(elem => elem.join(''));
+
+  for (let j = 0; j < flattened.length; j++) {
+      if (input.includes(flattened[j])) {
+        input = input.replace(flattened[j], '');
+        j = -1;
+      }
+  }
+
+  return input === '';
 }
